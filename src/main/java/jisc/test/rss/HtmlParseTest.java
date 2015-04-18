2df6f70929638a8ed45a74ab55557585dcf80851
Marcel Schwittlick <marzzzel@gmail.com>
2015-04-18 20:21:16 +0200

    adds html parse test

---
+++ src/main/java/jisc/test/rss/HtmlParseTest.java
@@ -0,0 +1,19 @@
+package jisc.test.rss;
+
+
+import jisc.general.EventSource;
+import jisc.htmlparse.JciEventsHtmlParser;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mrzl on 15.04.2015.
+ */
+public class HtmlParseTest {
+    @org.junit.Test
+    public void testRead() {
+        EventSource jciLondon = new EventSource( "JCI London", "http://www.jcilondon.org.uk/events/index.html" );
+        JciEventsHtmlParser parser = new JciEventsHtmlParser();
+        assertTrue( false );
+    }
+}
